9c6fecb53f6886c747b1d44e26b9b1c77fe19ea8
smk71 <37836494+smk71@users.noreply.github.com>
2018-04-08 20:14:56 +0100

    Add files via upload

---
+++ new 11.py
@@ -0,0 +1,77 @@
+def BookOptions():
+book_option = [[1, "Moby Dick (1851) ",15.20 ], [2, "The Terrible Privacy of Maxwell Sim (2010)", 13.14],
+    [3, "Still Life With Woodpecker (1980)", 11.05], [4, "Sleeping Murder (1976)", 10.24], [5, "Three Men in a Boat (1889)", 12.87],
+    [6, "The Time Machine (1895)", 10.43], [7, "The Caves of Steel (1954)", 8.12], [8, "Idle Thoughts of an Idle Fellow (1886) ", 7.32],
+    [9, "A Christmas Carol (1843) ", 4.23], [10, "A Tale of Two Cities (1859)", 
+    while True:
+        try:
+            select = int(input("Enter option number: ")
+            if select == 1:
+                Optionone()
+                break        
+            elif select == 2:
+                Option2()
+                break
+            elif select == 3:
+                Option3()
+                break  
+
+            elif select == 4:
+                Option4()
+                break  
+
+            elif select == 5:
+                Option5()
+                break
+            elif select == 6:
+                Option6()
+                break
+            elif select == 7:
+                Option7()
+                break  
+
+            elif select == 8:
+                Option8()
+                break  
+
+            elif select == 9:
+                Option9()
+                break
+            elif select == 10:
+                Option10()
+                break  
+
+            elif select == 11:
+                Option11()
+                break
+            else: 
+                print("Invalid. Enter 1-11")
+                BookOptions()
+         except ValueError:
+                print("Invalid. Enter 1-11")          
+    exit
+	class calculate
+	    x = 0.05*13.14
+	    one = 15.20
+		two = 13.14 + x
+		three = 11.05
+		four = 10.24
+		five = 12.87
+		six = 10.43
+		seven = 8.12
+		eight = 7.32
+		nine = 4.23
+		ten = 6.32
+		eleven = 13.21
+		
+		def description(cost):
+		    desc_str = (cost.one, cost.two, cost.three,cost.four,cost.five,cost.six,
+			cost.seven,cost.eight,cost.nine,cost.ten,cost.eleven)
+			return desc_str
+	class discount
+	
+	class total
+def Option1()
+    print(one)
+
+
